bcbe7770fafeb31b86d9d5c7d3ad0f281ac672d3
nik <isp06@mail.ru>
2021-09-18 22:47:24 +0300

    lesson-1

--- task-1/task_1.py
+++ task-1/task_1.py
@@ -1,3 +1,12 @@
+# 1. Написать функцию host_ping(), в которой с помощью
+# утилиты ping будет проверяться доступность сетевых узлов.
+# Аргументом функции является список, в котором каждый
+# сетевой узел должен быть представлен именем хоста или ip-адресом.
+# В функции необходимо перебирать ip-адреса и
+# проверять их доступность с выводом соответствующего сообщения
+# («Узел доступен», «Узел недоступен»).
+# При этом ip-адрес сетевого узла должен создаваться с помощью функции ip_address()
+
 from ipaddress import ip_address
 from subprocess import Popen, PIPE
 
@@ -5,23 +14,26 @@ from subprocess import Popen, PIPE
 import socket
 
 
-def host_ping(list_ip_adress, timeout=500, request=1):
+def host_ping(list_ip_address:list, timeout=10, request=1) -> None or list:
     """
-    :param list_ip_adress:
+    :param list_ip_address:
     :param timeout:
     :param request:
     :return:
     """
+    list_ip = []
     result = {'Доступные узлы': '', 'Недоступные узлы': ''}
-    for address in list_ip_adress:
+    for address in list_ip_address:
         try:
             address = ip_address(socket.gethostbyname(address))
             # address = ip_address(address)
 
         except ValueError:
-            pass
-            # address = socket.gethostbyname(address) #Можено
-        proc = Popen(f'ping {address} -w {timeout} -n {request}', shell=False, stdout=PIPE)
+             # pass
+            address = socket.gethostbyname(address) #Можено
+        #заменил флаг n  на c (для линукс)
+        args = ["ping", "-c", str(request), "-w", str(timeout), str(address)]
+        proc = Popen(args, shell=False, stdout=PIPE)
         proc.wait()
 
         if proc.returncode == 0:
@@ -31,8 +43,13 @@ def host_ping(list_ip_adress, timeout=500, request=1):
         else:
             result['Недоступные узлы'] += f'{str(address)}\n'
             res_string = f'{address}- Узел недоступен'
+        if __name__ == '__main__':
+             print(res_string)
+        else:
+            list_ip.append(list(res_string.split('-')))
 
-        print(res_string)
+    if __name__ != '__main__':
+        return  list_ip
 
 
 if __name__ == '__main__':
