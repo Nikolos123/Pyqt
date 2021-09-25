85fd38c2d617892b53e3cab1dd285e91e689e56d
nik <isp06@mail.ru>
2021-09-25 14:50:10 +0300

    update launcher

--- task-2/launcher.py
+++ task-2/launcher.py
@@ -1,8 +1,12 @@
 import os
+import random
 import subprocess
 import time
 
 process = []
+
+def get_name(i):
+    return  f'{random.getrandbits(128)}/{i}'
 
 while True:
     action = input('Выберите действие: q - выход , s - запустить сервер и клиенты, x - закрыть все окна:')
@@ -13,22 +17,16 @@ while True:
 
         clients_count = int(input('Введите количество тестовых клиентов для запуска: '))
         #start server
-        # catalog =  os.getcwd()
-        # path_server = f'python3 {catalog}/server.py'
         process.append(subprocess.Popen('gnome-terminal -- python3 server.py', shell=True))
 
-        # Запускаем сервер!
-        # process.append(subprocess.Popen(['python' ,'server.py'],stdout = subprocess.PIPE))
         # Запускаем клиентов:
         time.sleep(0.5)
         for i in range(clients_count):
-            process.append(subprocess.Popen(f'gnome-terminal -- python3 client.py -n Test{i}', shell=True))
-            time.sleep(0.1)
-            # process.append(
-            #     subprocess.Popen(['python','client.py',f'-n test{i + 1}'],stdout = subprocess.PIPE))
+            #Добавил так имя так как имена 1-2-3 бывают заняты
+            name = get_name(i)
+            process.append(subprocess.Popen(f'gnome-terminal -- python3 client.py -n Test{name}', shell=True))
     elif action == 'x':
         while process:
             victim = process.pop()
             victim.kill()
             victim.terminate()
-
