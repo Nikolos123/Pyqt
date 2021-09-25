5ad919ce7fc552002b39a672746c36b82abc4631
nik <isp06@mail.ru>
2021-09-25 14:54:37 +0300

    update launcher

--- task-2/launcher.py
+++ task-2/launcher.py
@@ -1,18 +1,30 @@
+import random
 import subprocess
+import time
 
 process = []
+def get_name(i):
+    return  f'{random.getrandbits(128)}/{i}'
 
 while True:
     action = input('Выберите действие: q - выход , s - запустить сервер и клиенты, x - закрыть все окна:')
+
     if action == 'q':
         break
     elif action == 's':
+
         clients_count = int(input('Введите количество тестовых клиентов для запуска: '))
-        # Запускаем сервер!
-        process.append(subprocess.Popen('python server.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
+        #start server
+        process.append(subprocess.Popen('gnome-terminal -- python3 server.py', shell=True))
+
         # Запускаем клиентов:
+        time.sleep(0.5)
         for i in range(clients_count):
-            process.append(subprocess.Popen(f'python client.py -n test{i + 1}', creationflags=subprocess.CREATE_NEW_CONSOLE))
+            #Добавил так имя так как имена 1-2-3 бывают заняты
+            name = get_name(i)
+            process.append(subprocess.Popen(f'gnome-terminal -- python3 client.py -n Test{name}', shell=True))
     elif action == 'x':
         while process:
-            process.pop().kill()
+            victim = process.pop()
+            victim.kill()
+            victim.terminate()
